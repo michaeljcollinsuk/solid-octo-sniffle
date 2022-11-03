1a9cacea8f6214de64a7184e6eeefa885f58d7d9
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2022-11-03 19:18:10 +0000

    Working

--- main.py
+++ main.py
@@ -1,27 +1,63 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 from collections import defaultdict
+from dataclasses import dataclass
 
 
+class OfferMixin:
+
+    quantity_required = None
+
+    def get_reduction(self, item_price, quantity):
+        raise NotImplementedError("Subclass must implement this method")
+
+
+class BOGOF(OfferMixin):
+
+    quantity_required = 2
+
+    def get_reduction(self, item_price, quantity):
+        if quantity < self.quantity_required:
+            return 0
+
+        items_free = int(quantity / self.quantity_required)
+        return item_price * items_free
+
+
+class Discount(OfferMixin):
+
+    def __init__(self, quantity_required, discount_percent):
+        super().__init__()
+        self.quantity_required = quantity_required
+        self.discount_percent = discount_percent
+
+    @property
+    def discount_percentage(self):
+        if not self.discount_percent:
+            return 0
+        return self.discount_percent / 100
+
+    def get_reduction(self, item_price, quantity):
+        if quantity < self.quantity_required:
+            return 0
+
+        discount_per_item = int(item_price * self.discount_percentage)
+        total_discount = quantity * discount_per_item
+        return total_discount
+
+
+@dataclass
 class Product:
 
-    def __init__(self, name, code, price, offer=None):
-        self.name = name
-        self.code = code
-        self.price = price
-        self.offer = None
+    name: str
+    code: str
+    price: int
+    offer: OfferMixin = None
 
 
 class Checkout:
 
-    products = []
-    sub_total = 0
-    # products = {
-    #     "pr1": 1,
-    #     "pr2": 2,
-    # }
+    def __init__(self):
+        self.products = []
+        self.sub_total = 0
 
     @property
     def quantities(self):
@@ -30,76 +66,66 @@ class Checkout:
             quantities[product.code] += 1
         return quantities
 
-    @property
-    def offers(self):
-        return {product.code: product.offer for product in self.products if product.offer}
-
     def scan(self, product):
         self.products.append(product)
         self.sub_total += product.price
 
-    @property
-    def get_total_reductions(self):
+    def calculate_discounts(self):
+        applied = []
         reductions = 0
-        for product_code, offer in self.offers.items():
-            print(offer)
-            reductions -= offer.get_reduction()
+        for product in self.products:
+            
+            offer = product.offer
+            if not offer:
+                continue
+
+            if product.code in applied:
+                continue
+
+            reduction = offer.get_reduction(
+                item_price=product.price,
+                quantity=self.quantities[product.code]
+            )
+            applied.append(product.code)
+            reductions += reduction
+
         return reductions
 
-    def calculate_total(self):
-        return self.sub_total - self.get_total_reductions
-
-
-class OfferMixin:
-
-    def __init__(self, product, quantity_required):
-        self.product = product
-        self.quantity_required = quantity_required
-
-    def get_reduction(self, quantity):
-        raise NotImplementedError("Subclass must implement this method")
-
-
-class BOGOF(OfferMixin):
-
-    def get_reduction(self, quantity):
-        items_free = int(quantity / self.quantity_required)
-        return self.product.price * items_free
-
-
-class Discount(OfferMixin):
-
-    discount_percent = 10
-
     @property
-    def discount_percentage(self):
-        return self.discount_percent / 100
+    def total(self):
+        return self.sub_total - self.calculate_discounts()
 
-    @property
-    def discount_per_item(self):
-        return int(self.product.price * self.discount_percentage)
+    def pretty_price(self, price):
+        decimal = price / 100
+        return f"£{decimal}"
 
-    def get_reduction(self, quantity):
-        if quantity < self.quantity_required:
-            return 0
+    def print_totals(self):
+        sub_total = self.pretty_price(self.sub_total)
+        total = self.pretty_price(self.total)
+        print(f"{sub_total=}")
+        print(f"{total=}")
 
-        total_discount = quantity * self.discount_per_item
-        return total_discount
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     checkout = Checkout()
-    tea1 = Product(name="Fruit tea", code="FR1", price=311)
-    tea2 = Product(name="Fruit tea", code="FR1", price=311)
-    BOGOF(quantity_required=1)
-    BOGOF(quantity_required=1)
+    bogof = BOGOF()
+    ten_percent = Discount(discount_percent=10, quantity_required=3)
 
-    checkout.scan(tea1)
-    checkout.scan(tea2)
-    print(checkout.sub_total)
-    print(checkout.calculate_total())
+    products = [
+        Product(name="Fruit tea", code="FR1", price=311, offer=bogof),
+        Product(name="Fruit tea", code="FR1", price=311, offer=bogof),
+        Product(name="Fruit tea", code="FR1", price=311, offer=bogof),
+        Product(name="Fruit tea", code="FR1", price=311, offer=bogof),
+        Product(name="Fruit tea", code="FR1", price=311, offer=bogof),
+        Product(name="Coffee", code="CF1", price=1123),
+        Product(name="Strawberries", code="SR1", price=500, offer=ten_percent),
+        Product(name="Strawberries", code="SR1", price=500, offer=ten_percent),
+        Product(name="Strawberries", code="SR1", price=500, offer=ten_percent),
+        Product(name="Strawberries", code="SR1", price=500, offer=ten_percent),
+        Product(name="Strawberries", code="SR1", price=500, offer=ten_percent),
+    ]
 
+    for prod in products:
+        checkout.scan(prod)
 
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    checkout.print_totals()
---
+++ tests.py
@@ -0,0 +1,70 @@
+import unittest
+
+from main import BOGOF, Discount, Product, Checkout
+
+
+class TestTotals(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.bogof = BOGOF()
+        self.ten_percent_discount = Discount(discount_percent=10, quantity_required=3)
+        self.checkout = Checkout()
+
+    def test_fruit_tea_buy_one_get_one_free(self):
+        """
+        Check that when two fruit tea products are scanned, the total is the price of one as the BOGOF offer is applied.
+        If a third is added to the basked, the price is increased, but the fourth item is also free
+        """
+        fruit_tea = Product(name="Fruit tea", code="FR1", price=311, offer=self.bogof)
+
+        self.checkout.scan(fruit_tea)
+        self.assertEqual(self.checkout.total, 311)
+
+        # second item should be free
+        self.checkout.scan(fruit_tea)
+        self.assertEqual(self.checkout.total, 311)
+
+        self.checkout.scan(fruit_tea)
+        self.assertEqual(self.checkout.total, 622)
+
+        self.checkout.scan(fruit_tea)
+        self.assertEqual(self.checkout.total, 622)
+
+    def test_strawberries_discounted_when_buying_three(self):
+        """
+        Check that when you buy three or more strawberries, the total is discounted by the correct amount.
+        """
+        strawberries = Product(name="Strawberries", code="SR1", price=500, offer=self.ten_percent_discount)
+
+        self.checkout.scan(strawberries)
+        self.assertEqual(self.checkout.total, 500)
+
+        self.checkout.scan(strawberries)
+        self.assertEqual(self.checkout.total, 1000)
+
+        # adding a third item discounts the items
+        self.checkout.scan(strawberries)
+        self.assertEqual(self.checkout.total, 1350)
+
+        # fourth item should also be discounted
+        self.checkout.scan(strawberries)
+        self.assertEqual(self.checkout.total, 1800)
+
+    def test_combination_of_products(self):
+        """
+        Check that when you buying a combination of both products, both discounts are applied to the total
+        """
+        strawberries = Product(name="Strawberries", code="SR1", price=500, offer=self.ten_percent_discount)
+        fruit_tea = Product(name="Fruit tea", code="FR1", price=311, offer=self.bogof)
+
+        self.checkout.scan(fruit_tea)
+        self.checkout.scan(fruit_tea)
+        self.checkout.scan(strawberries)
+        self.checkout.scan(strawberries)
+        self.checkout.scan(strawberries)
+
+        self.assertEqual(self.checkout.total, 1661)
+
+
+if __name__ == '__main__':
+    unittest.main()
